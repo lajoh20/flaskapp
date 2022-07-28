ee3133b7d8cd7e6326990082ddf468c3885c53d7
Ubuntu <ubuntu@ip-172-31-85-32.ec2.internal>
2022-07-28 18:41:48 +0000

    size commit

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,13 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for
 from app import app
 import os
+from app.import_csv import import_csv
 
 @app.route("/")
 def index():
+    return render_template("main.html")
 
-    # Use os.getenv("key") to get environment variables
-    app_name = os.getenv("APP_NAME")
+@app.route("/upload", methods= ['GET',"POST"] )
+def upload():
+    if request.method == 'POST':
+        pass
+    return render_template("upload.html")
 
-    if app_name:
-        return f"Can you see {app_name} ?"
-
-    return "Hello from Flask"
